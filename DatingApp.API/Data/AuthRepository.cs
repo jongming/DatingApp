d2002241095c8f67006369c0ea975f899763c349
Ben Tai <btai@ryee-macbook-pro.local>
2020-02-25 23:23:03 +0900

    LoginFix

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -32,7 +32,7 @@ namespace DatingApp.API.Data
             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 for (int i=0; i < computedHash.Length; i++){
-                    if (computedHash[i] != computedHash[i]) return false;
+                    if (computedHash[i] != passwordHash[i]) return false;
                 }
             }
             return true;
